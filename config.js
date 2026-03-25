2e92bad0013cf8517804b4e060d1310b39f906ad
Daver <daver@example.com>
2026-03-24 22:27:21 -0700

    Update API URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzNs7mGyZCRHiOWxNW4C9EJZIchtO9_N7tjHfeINqYsatPGNWwvuDTpUXTWPFV2bCTb/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbw-yzdTSEnsOacvfcxnc3KaBHbsQW1SPDiRsTFb7BCRjW6Qe-nHjqExvzF7Y6FjHj4RUw/exec',
     REFRESH_INTERVAL_MS: 60 * 60 * 1000,
     EVENT_HEIGHT_MULTIPLIER: 2,
     EVENT_MIN_HEIGHT: 24,
